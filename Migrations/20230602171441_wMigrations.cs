2bca97fb9d7df5b52e67d3c9cbbef5510972c697
Vanessa Rocha <87250768+NessaRocha@users.noreply.github.com>
2023-06-02 14:20:25 -0300

    .

---
+++ Migrations/20230602171441_wMigrations.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrabalhoPooBanco.Migrations
+{
+    /// <inheritdoc />
+    public partial class wMigrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Transacoes",
+                newName: "IdDeposito");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Transacoes",
+                newName: "IdTransacao");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IdDeposito",
+                table: "Transacoes",
+                newName: "id");
+
+            migrationBuilder.RenameColumn(
+                name: "IdTransacao",
+                table: "Transacoes",
+                newName: "Id");
+        }
+    }
+}
